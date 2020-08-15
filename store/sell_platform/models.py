20ec4fca2063733ad6bec27a43f4d47afadd0f8d
DESKTOP-VT3BMBO\leo90 <leo900223@gmail.com>
2020-08-15 11:18:10 +0800

    create account

--- store/sell_platform/models.py
+++ store/sell_platform/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class SellPlatform(models.Model):
+    Name = models.CharField(unique=True, max_length=50, verbose_name='Name')
+    Description = models.TextField(verbose_name='Description')
+    EditTime= models.DateTimeField(auto_now=True, verbose_name='Edit Time')
--- store/store/settings.py
+++ store/store/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sell_platform.apps.SellPlatformConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
